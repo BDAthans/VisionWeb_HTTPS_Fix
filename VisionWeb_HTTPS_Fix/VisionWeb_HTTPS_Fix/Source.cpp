58b891a97a706c743ab46f2432ed41a5f45464b8
0xShellcode <BDAthans@yahoo.com>
2019-03-01 16:42:39 -0600

    2nd Commit
    Updated to resolve URL not working and to fix formatting issues.
    Tested working on client workstations.

--- VisionWeb_HTTPS_Fix/VisionWeb_HTTPS_Fix/Source.cpp
+++ VisionWeb_HTTPS_Fix/VisionWeb_HTTPS_Fix/Source.cpp
@@ -37,7 +37,7 @@ TCHAR wInstalledVersion[80]; String InstalledVersion;
 TCHAR wBuild[80]; String Build;
 TCHAR wServicePack[80]; String ServicePack;
 
-String runningVersion = "v0.0.4";
+String runningVersion = "v0.0.7";
 
 int getOmate32();
 
@@ -104,7 +104,7 @@ int getOmate32() {
 }
 
 void header() {
-	cout << left << setw(10) << "Eyefinity Officemate VisionWeb HTTPS Fix Tool " << runningVersion;
+	cout << left << setw(10) << "Eyefinity Officemate VisionWeb HTTPS Login Fix Tool " << runningVersion;
 	cout << string(2, '\n');
 }
 
@@ -117,98 +117,153 @@ void updateVisionWebINI() {
 
 	fstream VisionWebINI;
 		VisionWebINI.open(filenamepath, fstream::out);
-		VisionWebINI << "[VisionWeb]\n";
-		VisionWebINI << "Available=Yes\n";
-		VisionWebINI << "Integrated=Yes\n";
-		VisionWebINI << "Graphic4Browser=VisionWeb.gif\n";
+		VisionWebINI << "[VisionWeb]";
+		VisionWebINI << "\n";
+		VisionWebINI << "Available=Yes";
+		VisionWebINI << "\n";
+		VisionWebINI << "Integrated=Yes";
+		VisionWebINI << "\n";
+		VisionWebINI << "Graphic4Browser=VisionWeb.gif";
+		VisionWebINI << "\n";
 		VisionWebINI << "\n";
 
-		VisionWebINI << "[VW Marketing Page]\n";
-		VisionWebINI << "URL=https://www.visionweb.com/officemate/officemate.html\n";
+		VisionWebINI << "[VW Marketing Page]";
+		VisionWebINI << "\n";
+		VisionWebINI << "URL=https://www.visionweb.com/officemate/officemate.html";
+		VisionWebINI << "\n";
 		VisionWebINI << "\n";
 
-		VisionWebINI << "[VWLogin]\n";
-		VisionWebINI << "URL=https://www.visionweb.com/servlet/com.visionweb.login.servlets.LoginServlet\n";
+		VisionWebINI << "[VWLogin]";
+		VisionWebINI << "\n";
+		//VisionWebINI << "URL=https://www.visionweb.com/login/login.jsp";
+		VisionWebINI << "URL=https://www.visionweb.com/servlet/com.visionweb.login.servlets.LoginServlet";
+		VisionWebINI << "\n";
 		VisionWebINI << "\n";
 
-		VisionWebINI << "[Patient Spectacle Order]\n";
-		VisionWebINI << "URL=https://www.visionweb.com/oerxlens/isapi/vwiisgeneric.dll/appentry.html\n";
+		VisionWebINI << "[Patient Spectacle Order]";
+		VisionWebINI << "\n";
+		VisionWebINI << "URL=https://www.visionweb.com/oerxlens/isapi/vwiisgeneric.dll/appentry.html";
+		VisionWebINI << "\n";
 		VisionWebINI << "\n";
 
-		VisionWebINI << "[Patient Contact Order]\n";
-		VisionWebINI << "URL=https://www.visionweb.com/oerxsoft/isapi/vwiisgeneric.dll/appentry_rx.html\n";
+		VisionWebINI << "[Patient Contact Order]";
+		VisionWebINI << "\n";
+		VisionWebINI << "URL=https://www.visionweb.com/oerxsoft/isapi/vwiisgeneric.dll/appentry_rx.html";
+		VisionWebINI << "\n";
 		VisionWebINI << "\n";
 
-		VisionWebINI << "[Inventory Contact Order]\n";
-		VisionWebINI << "URL=https://www.visionweb.com/oerxsoft/isapi/vwiisgeneric.dll/appentry_stk.html\n";
+		VisionWebINI << "[Inventory Contact Order]";
+		VisionWebINI << "\n";
+		VisionWebINI << "URL=https://www.visionweb.com/oerxsoft/isapi/vwiisgeneric.dll/appentry_stk.html";
+		VisionWebINI << "\n";
 		VisionWebINI << "\n";
 
-		VisionWebINI << ";THis url works for both patient and inventory(stock) orders\n";
-		VisionWebINI << "[Patient Frame Order]\n";
-		VisionWebINI << "URL=https://www.visionweb.com/frame/externalOrder.fr\n";
+		VisionWebINI << ";THis url works for both patient and inventory(stock) orders";
+		VisionWebINI << "\n";
+		VisionWebINI << "[Patient Frame Order]";
+		VisionWebINI << "\n";
+		VisionWebINI << "URL=https://www.visionweb.com/frame/externalOrder.fr";
+		VisionWebINI << "\n";
 		VisionWebINI << "\n";
 
-		VisionWebINI << "[Inventory Frame Order]\n";
-		VisionWebINI << "URL=https://www.visionweb.com/frame/externalOrder.fr\n";
+		VisionWebINI << "[Inventory Frame Order]";
+		VisionWebINI << "\n";
+		VisionWebINI << "URL=https://www.visionweb.com/frame/externalOrder.fr";
+		VisionWebINI << "\n";
 		VisionWebINI << "\n";
 
-		VisionWebINI << "[XML Headers]\n";
-		VisionWebINI << "Hdr1_XMLVersion=<?xml version=\"1.0\"?>\n";
-		VisionWebINI << "Hdr2_XMLStylesheetHref=<?xml:stylesheet type=\"text/xsl\" href=\"\n";
-		VisionWebINI << ";The path to the xsl file goes between Hdr2 and Hdr25\n";
-		VisionWebINI << "Header25_XMLHrefFName=VWDisplayOrder.xsl\"?>\n";
+		VisionWebINI << "[XML Headers]";
+		VisionWebINI << "\n";
+		VisionWebINI << "Hdr1_XMLVersion=<?xml version=\"1.0\"?>";
+		VisionWebINI << "\n";
+		VisionWebINI << "Hdr2_XMLStylesheetHref=<?xml:stylesheet type=\"text/xsl\" href=\"";
+		VisionWebINI << "\n";
+		VisionWebINI << ";The path to the xsl file goes between Hdr2 and Hdr25";
+		VisionWebINI << "\n";
+		VisionWebINI << "Header25_XMLHrefFName=VWDisplayOrder.xsl\"?>";
+		VisionWebINI << "\n";
 		VisionWebINI << "\n";
 
-		VisionWebINI << "[Order Status Request URL]\n";
-		VisionWebINI << "URL_1=https://www.visionweb.com/oesc/isapi/vwiisgeneric.dll/api?GUID=\n";
-		VisionWebINI << "URL_2=&ORDER=\n";
-		VisionWebINI << "URL_3=&do=details&xml=0\n";
-		VisionWebINI << ";;; 2 'GET' fields are needed: GUID(the sessionid) and ORDER(the order number)\n";
-		VisionWebINI << ";;;\n";
-		VisionWebINI << ";;; e.g.:\n";
-		VisionWebINI << ";;; https://www.visionweb.com/oesc/isapi/vwiisgeneric.dll/api?GUID={73DEE315-1A\n";
-		VisionWebINI << ";;; 4E-4905-B70D-6E30E57ACEBC}&ORDER=SP1b2z&do=details&xml=0\n";
+		VisionWebINI << "[Order Status Request URL]";
+		VisionWebINI << "\n";
+		VisionWebINI << "URL_1=https://www.visionweb.com/oesc/isapi/vwiisgeneric.dll/api?GUID=";
+		VisionWebINI << "\n";
+		VisionWebINI << "URL_2=&ORDER=";
+		VisionWebINI << "\n";
+		VisionWebINI << "URL_3=&do=details&xml=0";
+		VisionWebINI << "\n";
+		VisionWebINI << ";;; 2 'GET' fields are needed: GUID (the sessionid) and ORDER (the order number)";
+		VisionWebINI << "\n";
+		VisionWebINI << ";;;";
+		VisionWebINI << "\n";
+		VisionWebINI << ";;; e.g.:";
+		VisionWebINI << "\n";
+		VisionWebINI << ";;; https://www.visionweb.com/oesc/isapi/vwiisgeneric.dll/api?GUID={73DEE315-1A";
+		VisionWebINI << "\n";
+		VisionWebINI << ";;; 4E-4905-B70D-6E30E57ACEBC}&ORDER=SP1b2z&do=details&xml=0";
+		VisionWebINI << "\n";
 		VisionWebINI << "\n";
 
-		VisionWebINI << "[VisionWeb Time Out]\n";
-		VisionWebINI << "Minutes=120\n";
+		VisionWebINI << "[VisionWeb Time Out]";
 		VisionWebINI << "\n";
-		VisionWebINI << ";After an order is sent to pending or sent can show a msgbox with\n";
-		VisionWebINI << ";  confirmation of status from return xml along with order number.\n";
-
-		VisionWebINI << "[VisionWeb Status MsgBox]\n";
-		VisionWebINI << "Show=No\n";
+		VisionWebINI << "Minutes=120";
 		VisionWebINI << "\n";
-
-		VisionWebINI << "[Status Pending Orders Page]\n";
-		VisionWebINI << "URL=https://www.visionweb.com/oesc/isapi/vwiisgeneric.dll/ShoppingCart.html\n";
+		VisionWebINI << "\n";
+		VisionWebINI << ";After an order is sent to pending or sent can show a msgbox with";
+		VisionWebINI << "\n";
+		VisionWebINI << ";  confirmation of status from return xml along with order number.";
 		VisionWebINI << "\n";
 
-		VisionWebINI << "[Status Sent Orders Page]\n";
-		VisionWebINI << "URL=https://www.visionweb.com/oesc/isapi/vwiisgeneric.dll/Tracking.html\n";
+		VisionWebINI << "[VisionWeb Status MsgBox]";
+		VisionWebINI << "\n";
+		VisionWebINI << "Show=No";
+		VisionWebINI << "\n";
 		VisionWebINI << "\n";
 
-		VisionWebINI << "[Status Archived Orders Page]\n";
-		VisionWebINI << "URL=https://www.visionweb.com/oesc/isapi/vwiisgeneric.dll/ArchiveSearch.html\n";
+		VisionWebINI << "[Status Pending Orders Page]";
+		VisionWebINI << "\n";
+		VisionWebINI << "URL=https://www.visionweb.com/oesc/isapi/vwiisgeneric.dll/ShoppingCart.html";
+		VisionWebINI << "\n";
 		VisionWebINI << "\n";
 
-		VisionWebINI << "[Order Number Page Text]\n";
-		VisionWebINI << "URL_Text=Order\n";
+		VisionWebINI << "[Status Sent Orders Page]";
+		VisionWebINI << "\n";
+		VisionWebINI << "URL=https://www.visionweb.com/oesc/isapi/vwiisgeneric.dll/Tracking.html";
+		VisionWebINI << "\n";
 		VisionWebINI << "\n";
 
-		VisionWebINI << "[Order Details Page Text]\n";
-		VisionWebINI << "URL_Text=Details\n";
+		VisionWebINI << "[Status Archived Orders Page]";
+		VisionWebINI << "\n";
+		VisionWebINI << "URL=https://www.visionweb.com/oesc/isapi/vwiisgeneric.dll/ArchiveSearch.html";
+		VisionWebINI << "\n";
 		VisionWebINI << "\n";
 
-		VisionWebINI << "[Order Sent Page Text]\n";
-		VisionWebINI << "URL_Text=Send\n";
+		VisionWebINI << "[Order Number Page Text]";
+		VisionWebINI << "\n";
+		VisionWebINI << "URL_Text=Order";
+		VisionWebINI << "\n";
 		VisionWebINI << "\n";
 
-		VisionWebINI << "[Order Delete Page Text]\n";
-		VisionWebINI << "URL_Text=Delete\n";
+		VisionWebINI << "[Order Details Page Text]";
+		VisionWebINI << "\n";
+		VisionWebINI << "URL_Text=Details";
+		VisionWebINI << "\n";
 		VisionWebINI << "\n";
 
-		VisionWebINI << "[VW Error Msg]\n";
+		VisionWebINI << "[Order Sent Page Text]";
+		VisionWebINI << "\n";
+		VisionWebINI << "URL_Text=Send";
+		VisionWebINI << "\n";
+		VisionWebINI << "\n";
+
+		VisionWebINI << "[Order Delete Page Text]";
+		VisionWebINI << "\n";
+		VisionWebINI << "URL_Text=Delete";
+		VisionWebINI << "\n";
+		VisionWebINI << "\n";
+
+		VisionWebINI << "[VW Error Msg]";
+		VisionWebINI << "\n";
 		VisionWebINI << "Message=\"Please contact VisionWeb Customer Service at CustomerService@VisionWeb.com or by calling 1-800-874-6601.\"";
 
 		VisionWebINI.close();
